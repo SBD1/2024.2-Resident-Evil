94974811b607fe876519a39be0f04e28611c1a1c
Jose1277 <josee.oliveira022@gmail.com>
2025-02-02 18:25:01 -0300

    atualizado

--- jogo_java/src/main/java/application/Program.java
+++ jogo_java/src/main/java/application/Program.java
@@ -14,29 +14,27 @@ import static java.lang.System.exit;
 
 public class Program {
     public static void main(String[] args) throws InterruptedException, IOException {
-        while(true) {
-            Scanner input = new Scanner(System.in);
-            Menu.criarInicio();
-            Thread.sleep(1250);
-            Menu.limparTerminal();
+        Scanner input = new Scanner(System.in);
+        Menu.criarInicio();
+        Thread.sleep(1250);
+        Menu.limparTerminal();
 
-            Menu.criarMenu();
-            int opcao = input.nextInt();
-            switch(opcao){
-                case 1:
-                    System.out.println("Iniciando novo jogo...");
-                    Game.newgame();
-                    break;
+        Menu.criarMenu();
+        int opcao = input.nextInt();
+        switch(opcao){
+            case 1:
+                System.out.println("Iniciando novo jogo...");
+                Game.newgame();
+                break;
 
-                case 2:
-                    System.out.println("Carregando jogo...");
-                    break;
+            case 2:
+                System.out.println("Carregando jogo...");
+                break;
 
-                case 0:
-                    System.out.println("Saindo do jogo...");
-                    Thread.sleep(500);
-                    exit(0);
-            }
+            case 0:
+                System.out.println("Saindo do jogo...");
+                Thread.sleep(500);
+                exit(0);
         }
     }
 }
---
+++ jogo_java/src/main/java/retaguarda/inventario/InventarioService.java
@@ -0,0 +1,4 @@
+package retaguarda.inventario;
+
+public class InventarioService {
+}
